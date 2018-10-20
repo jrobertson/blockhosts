bd08217dfeb19fe008f426faf4e2b8ff6153f6dd
James Robertson <james@jamesrobertson.eu>
2018-10-20 23:56:08 +0100

    feature: A banlist can now be supplied to block multiple domains associated with 1 host.

--- lib/blockhosts.rb
+++ lib/blockhosts.rb
@@ -3,6 +3,8 @@
 # file: blockhosts.rb
 
 
+require 'rxfhelper'
+
 class Host
 
   @ip = '127.0.0.1'
@@ -16,25 +18,53 @@ class Host
     '/etc/hosts'
   end
 
-  def self.add(hostname)
-    s = "#{@ip} #{hostname}"
+  def self.add(hostname, banlist=nil, 
+               hashtag=hostname.sub(/^www./,'').gsub('-','').gsub('.','dot'))
+
+    s = if banlist then
+
+      hashtag = hostname.sub(/^#/,'')
+      list, _ = RXFHelper.read(banlist)
+      list.lines.map {|hostx| "#{@ip} #{hostx.chomp} ##{hashtag}" }.join("\n")
+
+    else
+      "#{@ip} #{hostname} ##{hashtag}"
+    end
+
     open(@file, 'a') { |f| f.puts s } unless File.read(@file).include? hostname
   end
 
-  def self.disable(hostname)
-    modify() {|s| s.sub(/^#(#{@ip}\s+#{hostname}[^$]+)$/,'\1')}
+  def self.disable(hashtag)   
+
+    modify() do |line|
+      line.gsub(/^#([^#]+##{hashtag.sub(/^#/,'')}[^$]+$)/,'\1')
+    end
+
   end
 
-  def self.enable(hostname)
-    modify() {|s| s.sub(/^(#{@ip}\s+#{hostname}[^$]+)$/,'#\1')}
+  def self.enable(hashtag)
+
+    modify() do |line|
+      
+      if line[0] == '#' then
+        line
+      else
+        line.gsub(/^([^^]+##{hashtag.sub(/^#/,'')}[^$]+$)/,'#\1')
+      end
+      
+    end
+
   end
+  
+  def self.block(hashtag)   self.disable(hashtag)  end  
+  def self.unblock(hashtag) self.enable(hashtag)   end
 
   def self.modify()
    
     return unless block_given?
 
     s = File.read(@file)
-    s2 = yield(s)
+    s2 = s.lines.map {|x| yield(x)}.join
     File.write(@file, s2) unless s == s2
 
   end
